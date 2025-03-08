f27246961fec3dea282fa98b840ab2405935554b
onetwohour <chan8645@naver.com>
2025-03-08 23:24:17 +0900

    remove maybe useless files

--- modules/WarningWindow/warning.cpp
+++
@@ -1,5 +0,0 @@
-#include "warning.h"
-
-void show_warning_dialog(const wchar_t* message) {
-    MessageBoxW(NULL, message, L"Warning", MB_ICONWARNING | MB_OK | MB_SYSTEMMODAL);
-}
--- modules/WarningWindow/warning.h
+++
@@ -1,9 +0,0 @@
-#ifdef WARNING_EXPORTS
-#define WARNING_API __declspec(dllexport)
-#else
-#define WARNING_API __declspec(dllimport)
-#endif
-
-#include <Windows.h>
-
-extern "C" WARNING_API void show_warning_dialog(const wchar_t* message);
